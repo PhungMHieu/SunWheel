f36bb54efc35b4ec610443403612c8a2bbfb54ad
PhungMHieu <kiritosad41@gmail.com>
2025-09-05 15:04:56 +0700

    Initial Commit

---
+++ SunSpinnerV4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SunSpinnerV4
+//
+//  Created by iKame Elite Fresher 2025 on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SunSpinnerV4/SunSpinnerV4App.swift
@@ -0,0 +1,17 @@
+//
+//  SunSpinnerV4App.swift
+//  SunSpinnerV4
+//
+//  Created by iKame Elite Fresher 2025 on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SunSpinnerV4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
